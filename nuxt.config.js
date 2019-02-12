2d09b8bd8cee2039ad5c89abf07d8aa1b99c8313
Faisal Ibrahim Hadiputra <faisalron@users.noreply.github.com>
2019-02-12 21:49:24 +0900

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ module.exports = {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
     ]
   },
 
