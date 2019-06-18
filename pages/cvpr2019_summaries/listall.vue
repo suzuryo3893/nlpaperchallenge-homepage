77dbea77ed1747f880194a19b5754bfb61260c7e
Ryota Suzuki <suzuryo3893@gmail.com>
2019-06-18 14:27:29 +0900

    add page for listing all titles of summaries

---
+++ pages/cvpr2019_summaries/listall.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-jumbotron header-level="4">
+      <template slot="header">
+        <b-container>
+          <div class="text-center">CVPR2019論文サマリ</div>
+        </b-container>
+      </template>
+    </b-jumbotron>
+
+    <b-container v-if="!isLoading">
+      <div class="mb-4">
+        <!--
+        <b-pagination
+          :value="1"
+          :total-rows="totalCount"
+          :per-page="5"
+          align="center"
+          @change="handleChangePage">
+        </b-pagination>
+        -->
+      </div>
+      <div class="mb-4" v-for="(summary, idx) in summaries" :key="idx">
+        <summary-card :summary="summary"/>
+      </div>
+      <!--
+      <b-pagination
+        :value="1"
+        :total-rows="totalCount"
+        :per-page="5"
+        align="center"
+        @change="handleChangePage">
+      </b-pagination>
+      -->
+    </b-container>
+    <p v-else class="text-center">
+      <font-awesome-icon class="mr-5" :icon="['fas', 'spinner']" pulse size="2x"/>
+    </p>
+  </div>
+</template>
+
+<script>
+import SummaryCard from "~/components/SummaryCard_cvpr2019.vue";
+import axios from "axios";
+
+export default {
+  components: {
+    SummaryCard
+  },
+  asyncData() {
+    let { content: summaries } = require(`~/static/data/cvpr2019_summaries/all.json`);
+    return {
+      summaries,
+      isLoading: false
+    }
+  },
+};
+</script>
